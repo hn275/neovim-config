1ce924645b5f981fae3fe4caa2eba8e85db82818
Hal <hn.nguyen275@gmail.com>
2022-08-12 01:04:52 -0700

    clean up some codes for Mac branch

--- lua/tree_sitter/init.lua
+++ lua/tree_sitter/init.lua
@@ -34,15 +34,6 @@ local autotag_filetypes = { -- enabled file types
 	"xml",
 	"markdown",
 }
-local skip_tags = { -- disable for these tags
-	"area",
-	"input",
-	"hr",
-	"br",
-	"hr",
-	"img",
-	"param",
-}
 
 -- context-commentstring configuration
 local context_commentstring_config = require("tree_sitter.ts_context_commentstring")
@@ -51,7 +42,6 @@ tree.setup({
 	ensure_installed = highlight_parsers, -- install all parsers name
 	highlight = {
 		enable = true, -- enabling highlight
-		additional_vim_regex_highlighting = true, -- may slow neovim, well shall see
 	},
 	indent = {
 		enable = true, -- enable auto indentation
