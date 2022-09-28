c2efb6a985a26458f7fae1869313e5fa073962d0
Hal <hn.nguyen275@gmail.com>
2022-09-27 20:07:12 -0700

    added clangd

--- lua/lsp-config/server/cpp.lua
+++ lua/lsp-config/server/cpp.lua
@@ -4,7 +4,7 @@ local opts = require("lsp-config.default")
 local capabilities = opts.capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require("lspconfig")["cssls"].setup({
+require("lspconfig")["clangd"].setup({
 	opts.default_settings(),
 	on_attach = opts.on_attach,
 	flags = opts.lsp_flags,
