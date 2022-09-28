73b90dba64c8e05cd1b51dff2b8b96d1ac0e01f8
Hal <haln_01@proton.me>
2022-09-27 22:36:25 -0700

    added bashlsp

---
+++ lua/lsp-config/server/bash.lua
@@ -0,0 +1,12 @@
+local opts = require("lsp-config.default")
+
+-- enable builtin snippet support
+local capabilities = opts.capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require("lspconfig")["bashls"].setup({
+	opts.default_settings(),
+	on_attach = opts.on_attach,
+	flags = opts.lsp_flags,
+	capabilities = capabilities,
+})
