1ea20f347ab3b4c395e8adeb5d5281eb06660247
Hal <haln_01@proton.me>
2022-10-02 22:47:18 -0700

    nord theme adjustmet

--- lua/themes/nord/init.lua
+++ lua/themes/nord/init.lua
@@ -1,10 +1,10 @@
 local opts = {
 	nord_contrast = true,
 	nord_borders = true,
-	nord_disable_background = false,
+	nord_disable_background = true,
 	nord_cursorline_transparent = false,
-	nord_enable_sidebar_background = false,
-	nord_italic = false,
+	nord_enable_sidebar_background = true,
+	nord_italic = true,
 	nord_uniform_diff_background = true, -- colorful background when used in diff
 }
 
@@ -12,7 +12,6 @@ for key, val in pairs(opts) do
 	vim.api.nvim_set_var(key, val)
 end
 
-vim.cmd([[colorscheme nord]])
-
+require("nord").set()
 -- setting lualine color theme
 require("themes.util.setlualine")("nord")
