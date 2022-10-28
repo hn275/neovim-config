45bde63d3aa92e4a37c6454f9b6c50f42793937c
Hal <haln_01@proton.me>
2022-10-28 00:49:04 -0700

    cleaned up null-ls

--- lua/lsp-config/null-ls/null-ls.lua
+++ lua/lsp-config/null-ls/null-ls.lua
@@ -8,24 +8,17 @@ end
 local formatting = null_ls.builtins.formatting
 -- local diagnostics = null_ls.builtins.diagnostics
 local codeAction = null_ls.builtins.code_actions
+
 null_ls.setup({
 	debounce = 500,
 	diagnostics_format = "[#{c}] #{m} (#{s})",
 	sources = {
 		-- Code actions
 		codeAction.eslint_d, -- js, jsx, ts, tsx
-		-- Diagnostics
-		-- diagnostics.eslint_d, -- js, jsx, ts, tsx
-		-- diagnostics.markdownlint, -- markdown
-		-- diagnostics.stylint, -- css, scss, sass, less
-		-- diagnostics.tsc, -- tsc compiler linter
-		-- diagnostics.golangci_lint, -- go
-		-- diagnostics.cpplint, -- cpp
 		-- Formatting
 		formatting.black, -- python
 		formatting.markdownlint, -- markdown
 		formatting.prettierd, -- js, jsx, ts, tsx, html, yaml
 		formatting.stylua, -- lua
-		-- formatting.clang_format, -- lua
 	},
 })
