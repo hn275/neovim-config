9d1a77264aff2078d7bc4636a8e2a19895f1158d
Hal <haln_01@proton.me>
2022-11-14 23:10:08 -0800

    fix: luatab -> tabline

--- lua/statusline/init.lua
+++ lua/statusline/init.lua
@@ -1,11 +1,16 @@
 -- local lualine_ok, lualine = pcall(require, "lualine")
-local tabline_ok, tabline = pcall(require, "luatab")
+local tabline_ok, tabline = pcall(require, "tabline")
 
 if not tabline_ok then
 	print("failed to load tabline")
 else
 	tabline.setup({
 		show_index = true,
+		modified_icon = "[+]",
+		close_icon = "",
+		show_icon = false,
+		separator = "|",
+		padding = 1,
 	})
 end
 
