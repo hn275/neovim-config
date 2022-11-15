21ba921a8846701a1a1b36144930e72b8e6b2675
Hal <haln_01@proton.me>
2022-11-15 12:08:05 -0800

    fix: fzflua

--- lua/fuzzyfinder/fzf.lua
+++ lua/fuzzyfinder/fzf.lua
@@ -1,10 +1,21 @@
-local fzf_ok, _ = pcall(require, "fzf-lua")
+local fzf_ok, fzf = pcall(require, "fzf-lua")
 if not fzf_ok then
 	error("FAILED: fzf-lua not found")
 	return
 end
 
-local k = require("util.keymap").keymap
+fzf.setup({
+	winopts = {
+		split = "belowright new",
+		border = "single",
 
+		preview = {
+			wrap = "wrap",
+			layout = "horizontal",
+		},
+	},
+})
+
+local k = require("util.keymap").keymap
 k("n", "<leader>ff", "<CMD>FzfLua files<CR>")
 k("n", "<leader>fg", "<CMD>FzfLua live_grep_resume<CR>")
