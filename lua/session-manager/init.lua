6f8435d63c79be320fdd5124f29419895323c5f7
HalN <haln_01@proton.me>
2023-01-02 12:20:32 -0800

    fix: data path for sessions

--- lua/session-manager/init.lua
+++ lua/session-manager/init.lua
@@ -1,8 +1,31 @@
 local k = require("util.keymap").keymap
 
-k("n", "<C-s>s", ":mksession! ~/.nvim/sessions/", { noremap = true })
-k("n", "<C-s>l", ":source ~/.nvim/sessions/", { noremap = true })
+local path = vim.fn.stdpath("data") .. "/sessions"
+
+-- check for file existence at path/sessions
+local f = io.popen("ls " .. path)
+
+if f == nil then
+	error("Can't execute zsh command")
+	return
+end
+
+local output = f:read("*a")
+if output ~= nil then
+	local handle = io.popen("mkdir -p " .. path)
+	if handle == nil then
+		error("Cant create a new dir")
+	end
+end
+
+-- key map
+k("n", "<C-s>s", ":mksession! " .. path .. "/ ", { noremap = true }) -- TODO: change this to path, add NUI
+k("n", "<C-s>l", "<CMD>source " .. path .. "/.cache_session.vim<CR>", { noremap = true })
 k("n", "<C-s>d", ":!rm ~/.nvim/sessions/", { noremap = true })
 
-vim.api.nvim_create_user_command("SessionSave", ":mksession! ~/.nvim/sessions/.prev_session", {})
-vim.api.nvim_create_user_command("SessionLoad", ":source ~/.nvim/sessions/.prev_session", {})
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+	pattern = { "*.*" },
+	callback = function()
+		vim.cmd("mksession! " .. path .. "/.cache_session.vim")
+	end,
+})
